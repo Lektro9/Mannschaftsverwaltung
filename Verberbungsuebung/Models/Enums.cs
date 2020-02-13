a92938e1bfc540bc7fd0db19621ac18870d94671
kroll_lars <kroll_lars@i010-25>
2020-02-13 11:17:10 +0100

    change namespace of project

--- Verberbungsuebung/Models/Enums.cs
+++ Verberbungsuebung/Models/Enums.cs
@@ -11,7 +11,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace Verberbungsuebung
+namespace Mannschaftsverwaltung
 {
     public enum Turnierstatus
     {
--- Verberbungsuebung/Models/Physiotherapeut.cs
+++ Verberbungsuebung/Models/Physiotherapeut.cs
@@ -10,7 +10,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace Verberbungsuebung
+namespace Mannschaftsverwaltung
 {
     class Physiotherapeut : Person
     {
